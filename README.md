1d0b02f3ef368bace33175a3461c91cae5d98083
Jeff <jmunozcastro16@gmail.com>
2021-06-17 22:55:39 -0600

    Terminada primera versión del programa

---
+++ README.md
@@ -0,0 +1,31 @@
+# Métodos de ordenación en Java
+
+Este programa ordena un arreglo de 1000 (mil) posiciones con números aleatorios entre el 0 al 10.000 (diez mil).
+
+## Métodos de ordenación utilizados
+
+* Burbuja
+* QuickSort
+* ShellSort
+
+##  Consideraciones
+
+* Realizado en lenguaje de programación Java
+* JDK 8
+* Netbeans 8.2
+* Windows 10
+
+## Capturas
+
+**Menú principal**
+
+![Menú principal](Menu.PNG)
+
+**Generación del arreglo desordenado**
+
+![Arreglo desordenado](Arreglo.PNG)
+
+**Arreglo ordenado y tiempo de ejecución en milisegundos**
+
+![Arreglo ordenado](Ordenado.PNG)
+
---
+++ src/estructuras_proyecto1/Funciones.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuras_proyecto1;
+
+/**
+ *
+ * @author Jeffrey
+ */
+public class Funciones {
+    
+    public int [] burbuja(int [] arreglo){
+        int tamanio = arreglo.length;
+        //for externo que recorre todo el arreglo
+        for (int i = 0; i < tamanio-1; i++){
+            //for interno que recorre y compara los demás valores
+            for (int j = 0; j < tamanio-i-1; j++){
+                 //si el valor actual es mayor al que sigue
+                if (arreglo[j] > arreglo[j+1])
+                {
+                    //se guarda el valor en una variable temporal
+                    //se cambian los valores (el valor siguiente será el actual)
+                    //y el que era el valor sigiente se le pasa la variable temporal
+                    int temp = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = temp;
+                }
+            }
+               
+        }
+        
+        //se retorna el arreglo ordenado
+        return arreglo;
+            
+    }
+    
+    //comienza a ordenar de forma recursiva
+    public int [] quickSort(int [] arreglo, int bajo, int alto){
+         if (bajo < alto) { 
+            //obtiene el pivote para comenzar a ordenar
+            int pivote = particion(arreglo, bajo, alto); 
+   
+            // ordena cada partición de forma recursiva 
+            quickSort(arreglo, bajo, pivote-1); 
+            quickSort(arreglo, pivote+1, alto); 
+        } 
+         //devuelve el arreglo ordenado
+         return arreglo;
+    }
+    
+    public int particion(int[] arreglo, int bajo, int alto){
+        int pivote = arreglo[alto];  
+        int i = (bajo-1); // índice del elemento más bajo  
+        for (int j=bajo; j<alto; j++) { 
+            // verifica que el elemento sea menor o igual al pivote 
+            if (arreglo[j] <= pivote) { 
+                i++; 
+                // cambia arreglo[i] por arreglo[j] 
+                int temp = arreglo[i]; 
+                arreglo[i] = arreglo[j]; 
+                arreglo[j] = temp; 
+            } 
+        } 
+   
+        // cambia arreglo[i+1] y arreglo[alto] (o pivote) 
+        int temp = arreglo[i+1]; 
+        arreglo[i+1] = arreglo[alto]; 
+        arreglo[alto] = temp; 
+   
+        return i+1; 
+    }
+    
+    public int [] shellSort(int [] arreglo){
+        int salto, aux, i;
+        boolean cambios;
+  
+        for (salto = arreglo.length / 2; salto != 0; salto /= 2) {
+            cambios = true;
+            // Mientras se intercambie algún elemento 
+            while (cambios) {
+                //bo hay cambio aún
+                cambios = false;
+                // se recorre el resto de elementos
+                for (i = salto; i < arreglo.length; i++)   
+                {
+                    //si están desordenados se reordenan
+                    if (arreglo[i - salto] > arreglo[i]) {       
+                        aux = arreglo[i];                  
+                        arreglo[i] = arreglo[i - salto];
+                        arreglo[i - salto] = aux;
+                        // y se indica que se realizó el cambio
+                        cambios = true;                                                 
+                    }
+                }
+            }
+        }
+        //se retorna el arreglo ordenado
+        return arreglo;
+    }
+}
